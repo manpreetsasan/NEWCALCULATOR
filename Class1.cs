885180bac0832fc0c465a8298b7d6aee97427b8a
manpreetsasan <50706112+manpreetsasan@users.noreply.github.com>
2019-05-23 11:53:02 -0400

    Add files via upload
    division changes

--- Class1.cs
+++ Class1.cs
@@ -159,6 +159,19 @@ namespace Calculator
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+        [Test]
+        public void GetDivision_Input100tand20Returns5()
 
+        {
+            //Arrange
+            double number1 = 100;
+            double number2 = 20;
+            double expectedResult = number1 / number2;
+            Calc testCalc = new Calc(number1, number2);
+            //Act
+            double actualResult = testCalc.GetDivision();
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
