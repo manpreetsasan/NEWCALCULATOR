55a39e85ff0e55d2b033e4ffb17968c77934dc27
manpreetsasan <50706112+manpreetsasan@users.noreply.github.com>
2019-05-23 11:07:44 -0400

    Add files via upload
    Addition test commited

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using AwesomeCalculator;
+
+namespace Calculator
+{
+    public class Class1
+    {
+
+        [TestFixture]
+        class CalcTests
+        {
+            [Test]
+            public void GetAddition_Input20and50_Returns70()
+            {
+                //Arrange
+                double number1 = 20;
+                double number2 = 50;
+                double expectedResult = number1 + number2;
+                Calc testCalc = new Calc(number1, number2);
+                //Act
+                double actualResult = testCalc.GetAddition();
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+    }
+}
